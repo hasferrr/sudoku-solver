f0a849eb65c8139364bac0fcbad036c650e2ba5c
hasferrr <79697518+hasferrr@users.noreply.github.com>
2023-06-02 17:05:43 +0700

    implements validrow validcol

--- scripts.js
+++ scripts.js
@@ -112,10 +112,73 @@ function deepCopyArray(arr) {
     return arr.map(subArray => subArray.slice());
 }
 
+
 /**
  * @param {Board[]} lobd
  * @returns {Board[]}
  */
 function keepOnlyValid(lobd) {
-    return []; // TODO !!!
+    return lobd.filter(isValidBoard);
+}
+
+/**
+ * @param {Board} bd
+ * @returns {boolean}
+ */
+function isValidBoard(bd) {
+    return validRow(bd) && validColumn(bd) && validBox(bd);
+}
+
+/**
+ * @param {Board} bd
+ * @returns {boolean}
+ */
+function validRow(bd) {
+    for (let i = 0; i < bd.length; i++) {
+        const row = bd[i];
+        if (hasDuplicate(row)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+/**
+ * @param {any[]} arr
+ */
+function hasDuplicate(arr) {
+    const uniqueSet = new Set(arr);
+    return uniqueSet.size !== arr.length;
+}
+
+/**
+ * @param {Board} bd
+ * @returns {boolean}
+ */
+function validColumn(bd) {
+    /**
+     * for each column 1, 2, 3, ..., SIZE
+     * - make new array
+     * - check valid with hasDuplicate
+     * - if not valid, return false
+     * return true
+     */
+    const transposedBoard = transposeArray(bd);
+    return validRow(transposedBoard);
+}
+
+/**
+ * @param {Board} arr
+ * @returns {Board}
+ */
+function transposeArray(arr) {
+    return arr[0].map((_, columnIndex) => arr.map(row => row[columnIndex]));
+}
+
+/**
+ * @param {Board} bd
+ * @returns {boolean}
+ */
+function validBox(bd) {
+    return false; // TODO !!!
 }
