59e66d6c21d6b2f793546df3857b3a48b00a97a8
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-08-24 18:07:37 +0700

    chore: migrate to typescript
    javascript is stupid

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.js'
 import './index.css'
-import { GridContextProvider } from './contexts/GridContext'
+import { GridContextProvider } from './contexts/GridContext.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GridContextProvider>
       <App />
